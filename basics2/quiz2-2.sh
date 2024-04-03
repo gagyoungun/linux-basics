ed150857268443dc45708f57146451729a4a0ba2
root <@gmail.com>
2024-04-03 16:58:01 +0900

    2-4까지 했어요

---
+++ basics2/quiz2-2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+touch original.txt
+cp original.txt copy.txt
+
+ls -l original.txt
+ls -l copy.txt
+
+
+
+
+
+
---
+++ basics2/quiz2-4.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir myDir
+touch myFile.txt
+mv myFile.txt myDir
+
+rm -r myDir
+
