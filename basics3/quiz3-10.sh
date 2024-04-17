de1dfc6baf74ceb143b624415d580f677697734e
root <@gmail.com>
2024-04-17 12:37:29 +0900

    041고기먹고 싶다.

--- basics3/quiz3-10.sh
+++ basics3/quiz3-10.sh
@@ -2,7 +2,8 @@
 file_count=0
 dir_count=0
 
-for item in *; do
+for item in *
+ do
     if [ -f "$item" ]; then
         file_count=$((file_count + 1))
     elif [ -d "$item" ]; then
--- iffor/for.sh
+++ iffor/for.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for item in *
+for i in 1 2 3 4 5
 do
-		echo "$item"
+	echo "현재 숫자는 $i 입니다."
 done
 exit 0
